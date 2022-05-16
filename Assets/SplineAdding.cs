9296d6cd3096813dd498adc74ac89a16e4e5796d
Deniz Kaymakci <deniztkaymakci@gmail.com>
2022-05-16 17:26:38 +0300

    Forever Asset Added

---
+++ Assets/SplineAdding.cs
@@ -0,0 +1,15 @@
+using Dreamteck.Splines;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplineAdding : MonoBehaviour
+{
+    SplineComputer splineComputer;
+
+    public SplineComputer SplineComputer { get { return splineComputer == null ? splineComputer = GetComponent<SplineComputer>() : splineComputer; } }
+
+    void SplineAdd()
+    { SplineComputer.SetPoint( }
+
+}
