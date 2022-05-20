e4f21921140f147ddfeb00a810ebaaeeec59ae7f
denoboi <deniz.kaymakci@hotmail.com>
2022-05-20 15:35:29 +0300

    Environment Change(Again)

--- Assets/[Marathon]/Scripts/Runtime/AICollision.cs
+++ Assets/[Marathon]/Scripts/Runtime/AICollision.cs
@@ -51,12 +51,10 @@ public class AICollision : MonoBehaviour
 
         yield return new WaitForSeconds(3);
 
-        if(Input.GetMouseButton(0))
-        {
             SplineCharacter.CanMoveForward = true;  //burada kucuk bir bug var onemli olmayabilir. eger player duserse bekledikten sonra biz tiklamadan kosmaya devam ediyor. (Aslinda onemli tiklamadigim icin stamina dusmuyor)
             SplineCharacterAnimationController.TriggerAnimation("Run");
             SplineCharacter.IsControlable = true;
-        }
+      
        
 
         
