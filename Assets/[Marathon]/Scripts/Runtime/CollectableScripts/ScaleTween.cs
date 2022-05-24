d4df83f0d4794c379a9e88a630c93ce51b02b04d
denoboi <deniz.kaymakci@hotmail.com>
2022-05-24 16:52:22 +0300

    FinishLine + TapToStart
    DeleteSplineFollower

---
+++ Assets/[Marathon]/Scripts/Runtime/CollectableScripts/ScaleTween.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using HCB.UI;
+
+public class ScaleTween : MonoBehaviour
+{
+    void Start()
+    {
+        transform.DOScale(Vector3.one * 1.2f, 1f).SetLoops(-1, LoopType.Yoyo);
+    }
+}
